c188e5567df9f02c7f9122c60bbf7302853e1f82
KisnaAgarwal <opkisna1@gmail.com>
2026-07-02 01:16:02 +0530

    Update Host interface display and subnet mask handling

--- src/Host.cpp
+++ src/Host.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
-#include <Host.hpp>
-#include <vector>
 #include <sys/types.h>
 #include <sys/socket.h>
-
 #include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#include "Host.hpp"
 
 using namespace std;
 
@@ -30,14 +28,54 @@ void Host::showInterface(){
 
     while(curr!=NULL){
         cout<<"Interface :"<<curr->ifa_name<<endl;
+
+
         if(curr->ifa_addr != nullptr){
             if(curr->ifa_addr->sa_family==AF_INET) { //check if the network interface address is same as IP4
-                 sockaddr_in * ip4adr = (sockaddr_in*)curr->ifa_addr;
-                 char ipaddress [16];
-                 inet_ntop(AF_INET,&ip4adr->sin_addr ,ipaddress ,16);
-                 cout<< "ip address :" <<ipaddress<<endl;
+                //for IPv4 address
+                 sockaddr_in * ip4adr = (sockaddr_in*)curr->ifa_addr; //typecaast into sockaddr_in 
+                 char ipaddress [INET_ADDRSTRLEN];
+                 if(inet_ntop(AF_INET,&ip4adr->sin_addr ,ipaddress ,INET_ADDRSTRLEN)){ //convert from binary to 
+                     cout<< "IPv4 :" <<ipaddress<<endl;
+                 }else{
+                    perror("getifaddrs");
+                 }
+            }else if (curr->ifa_addr->sa_family ==AF_INET6){ //for IPv6 address
+                sockaddr_in6 * ip6adr = (sockaddr_in6*)curr->ifa_addr;
+                char ipaddress [INET6_ADDRSTRLEN];
+                if(inet_ntop(AF_INET6 ,&ip6adr->sin6_addr , ipaddress ,INET6_ADDRSTRLEN )){
+                    cout<<"IPv6 :"<<ipaddress<<endl;
+                }else{
+                    perror("getifaddrs");
+                }
             }
         }
+        //subnet mask : 
+            if(curr->ifa_netmask != nullptr){
+                //for ipv4
+                if(curr->ifa_netmask->sa_family==AF_INET){
+                    sockaddr_in * subnet_v4 = (sockaddr_in*)curr->ifa_netmask;
+                    char subnet_mask [INET_ADDRSTRLEN];
+                    if(inet_ntop(AF_INET,&subnet_v4->sin_addr ,subnet_mask ,INET_ADDRSTRLEN)){ //convert binary to 
+                        cout<<"Subnet Mask :"<<subnet_mask<<endl;
+                    }else{
+                        perror("inet_ntop");
+                    }
+                }
+                // for ipv6
+                 else if(curr->ifa_netmask->sa_family==AF_INET6){
+                    sockaddr_in6 * subnet_v6 = (sockaddr_in6*)curr->ifa_netmask;
+                    char subnet_mask [INET6_ADDRSTRLEN];
+                    if(inet_ntop(AF_INET6,&subnet_v6->sin6_addr ,subnet_mask ,INET6_ADDRSTRLEN)){
+                        cout << "Family: " << curr->ifa_addr->sa_family << endl;
+                        cout<<"Subnet Mask :"<<subnet_mask<<endl;
+                    }else{
+                        perror("inet_ntop");
+                    }
+                }
+            }
+
+
         curr = curr->ifa_next;
     }
     freeifaddrs(ptr);
